da5d49b5157fce3fd31ddd1c3e64558349384984
svc825 <sandravegacontreras@hotmail.com>
2025-03-14 09:56:18 -0500

    Agregada configuración ROOT

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,7 @@ DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = 'core.erp.urls'
 
 
 PORT = os.getenv('PORT', '10000')  # Render detectó el puerto 10000
