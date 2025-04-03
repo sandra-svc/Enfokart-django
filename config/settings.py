6f2d3d1023d2c2c8ea62e87f1e7ee252302fb289
sandra-svc <75272064+sandra-svc@users.noreply.github.com>
2025-04-03 15:33:21 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,6 +3,14 @@ import dj_database_url
 import locale
 
 
+os.environ["LANG"] = "es_CO.UTF-8"
+os.environ["LC_ALL"] = "es_CO.UTF-8"
+
+try:
+    locale.setlocale(locale.LC_ALL, "es_CO.UTF-8")
+except locale.Error:
+    locale.setlocale(locale.LC_ALL, "")
+
 # Build paths inside the project
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -105,13 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
-os.environ["LANG"] = "C.UTF-8"
-os.environ["LC_ALL"] = "C.UTF-8"
 
-try:
-    locale.setlocale(locale.LC_ALL, "C.UTF-8")
-except locale.Error:
-    locale.setlocale(locale.LC_ALL, "")
 
 # Internationalization
 LANGUAGE_CODE = 'es-CO'
