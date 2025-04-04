ba64d2be614cf960a0cf3259a28d31572355884e
sandra-svc <75272064+sandra-svc@users.noreply.github.com>
2025-04-04 09:37:14 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'babel',    
     'widget_tweaks',
     'core.erp',
     'core.homepage',
