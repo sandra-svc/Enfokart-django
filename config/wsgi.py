323fb155a7ae6b20aaf2ee5d08e1fd061f2a6b4b
sandra-svc <75272064+sandra-svc@users.noreply.github.com>
2025-04-09 14:03:40 -0500

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,4 @@
 import os
-import locale
-
-# Intenta establecer un locale compatible con Babel
-try:
-    locale.setlocale(locale.LC_ALL, 'es_CO.UTF-8')  # o 'en_US.UTF-8' si prefieres inglés
-except locale.Error:
-    print("No se pudo establecer 'es_CO.UTF-8', intentando con 'C.UTF-8'")
-    try:
-        locale.setlocale(locale.LC_ALL, 'C.UTF-8')
-    except locale.Error:
-        print("No se pudo establecer ningún locale completo, el formato de moneda puede fallar")
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
