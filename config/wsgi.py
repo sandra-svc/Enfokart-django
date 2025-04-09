51cad81d6a9901c5972d36bf34820f534335730a
sandra-svc <75272064+sandra-svc@users.noreply.github.com>
2025-04-09 10:31:06 -0500

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
+import locale
+
+# Intenta establecer un locale compatible con Babel
+try:
+    locale.setlocale(locale.LC_ALL, 'es_CO.UTF-8')  # o 'en_US.UTF-8' si prefieres inglés
+except locale.Error:
+    print("No se pudo establecer 'es_CO.UTF-8', intentando con 'C.UTF-8'")
+    try:
+        locale.setlocale(locale.LC_ALL, 'C.UTF-8')
+    except locale.Error:
+        print("No se pudo establecer ningún locale completo, el formato de moneda puede fallar")
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 application = get_wsgi_application()
+
