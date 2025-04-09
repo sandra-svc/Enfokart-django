f2affdf40f60a57cb8e4315d7f57042a4fc51567
sandra-svc <75272064+sandra-svc@users.noreply.github.com>
2025-04-09 11:07:49 -0500

    Update utils.py

--- core/erp/views/sale/utils.py
+++ core/erp/views/sale/utils.py
@@ -1,11 +1,15 @@
 # core/erp/views/sale/utils.py
+from babel.core import Locale
 from babel.numbers import format_currency
 
 def safe_format_currency(value):
     try:
-        return format_currency(value, 'COP', locale='es_CO')
+        # Creamos el locale explícitamente para que no dependa del sistema
+        locale = Locale.parse('es_CO')
+        return format_currency(value, 'COP', locale=locale)
     except Exception as e:
-        return f"${value:,.2f}".replace(",", ".").replace(".", ",", 1)  # fallback manual
+        return f"${value:,.2f}".replace(",", ".").replace(".", ",", 1)  # fallback
+
 
 
 
